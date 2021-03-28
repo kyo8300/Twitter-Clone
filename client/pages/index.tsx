2fbe1a19e0fa88b92032ec5c9670a3006adce821
unknown <kametyou777@gmail.com>
2021-03-29 01:16:02 +0800

    :wrench: svg configuration

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
 
-const IndexPage = () => <div className="container bg-red-100">Hi</div>
+import Example from 'components/Example'
+
+const IndexPage = () => (
+  <div className="container bg-red-100">
+    <Image src="/twitter-logo-blue.svg" width={600} height={600} />
+    <Example />
+  </div>
+)
 
 export default IndexPage
